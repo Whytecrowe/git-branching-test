9f32805cb3a41b92328b1afbe3dc17e9369e63b6
Kirill Korchagin <dor7an@gmail.com>
2021-06-08 17:10:22 -0700

    add nom code

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const print = require('common');
+const print = require('./common');
+const nomPrint = require('./nom');
 
 function main () {
   console.log(`
   APP HAS LAUNCHED!
   `);
   print();
+  nomPrint();
 }
 
 main();
